97535b827f12ddedc86a9abae44ba3391fad8ece
SE-My-Organizer <83239788+SE-My-Organizer@users.noreply.github.com>
2021-05-01 04:27:32 +0300

    Delete MyOrganizer directory

--- MyOrganizer/Configuration.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-// $con = mysqli_connect("sql104.byethost7.com","b7_28263837","Org123456789") or die("Error: Can't Connect to Server");
-// $db = mysqli_select_db($con,"b7_28263837_users") or die("Error: Can't Connect to DB");
-
-$con = mysqli_connect("sql104.byethost7.com","b7_28263837","Org123456789") or die("Error: Can't Connect to Server");
-$db = mysqli_select_db($con,"b7_28263837_users") or die("Error: Can't Connect to DB");
-
-?>
